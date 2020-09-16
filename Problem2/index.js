9906dfa18da92855579bdbd5bbaf574336eb9bad
A681910 <yatender.prajapati@atos.net>
2020-09-16 13:07:25 +0530

    Code Master v_3.0

--- Problem2/index.js
+++ Problem2/index.js
@@ -4,7 +4,6 @@ for(var i=1; i<res.length; i++){
     var num = res[i];
     while(num > 0){
         var splitNum = num.toString().split("");
-        console.log(splitNum);
             var f = 1;
             for(var j=0; j<splitNum.length-1; j++){
                 if(splitNum[j+1] < splitNum[j]){
