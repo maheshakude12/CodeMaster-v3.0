36ec813604a99c591eeb6e2eceb4074ccadc31d6
Rumao <aaron-pascoal.rumao@atos.net>
2020-09-16 12:25:57 +0530

    CM3- try 1

--- Problem2/solution/src/main/scala/com/codemaster/solution/Solution.scala
+++ Problem2/solution/src/main/scala/com/codemaster/solution/Solution.scala
@@ -2,12 +2,19 @@ package com.codemaster.solution
 
 import java.io.{File, PrintWriter}
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 object Solution {
 
-  def getLuckyNumber(a: Int): Int = {
-    0
+  @tailrec
+  def getLuckyNumber(num: Long): Long = {
+    val listOfDigits = num.toString.split("").toSeq
+    if (listOfDigits.equals(listOfDigits.sorted)) {
+      num
+    } else {
+      getLuckyNumber(num - 1)
+    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -17,10 +24,9 @@ object Solution {
     var count = 0
     input.drop(1).foreach(x => {
       count = count + 1
-      output.write(s"Request-$count: ${
-        getLuckyNumber(x.toInt)
-      }\n")
+      output.write(s"Request-$count: ${getLuckyNumber(x.toLong)}\n")
     }
     )
+    output.close()
   }
 }
